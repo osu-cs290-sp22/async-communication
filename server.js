b04945cf4e262079ca78200a275125690ff345d4
Rob Hess <hessro@oregonstate.edu>
2022-05-25 11:41:21 -0700

    Add code from 5/25 lecture.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var fs = require('fs')
 var express = require('express')
 var exphbs = require('express-handlebars')
 
@@ -8,6 +9,8 @@ var port = process.env.PORT || 8000
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(express.json())
 
 app.use(express.static('public'))
 
@@ -25,6 +28,35 @@ app.get('/people/:person', function (req, res, next) {
   var person = req.params.person.toLowerCase()
   if (peopleData[person]) {
     res.status(200).render('photoPage', peopleData[person])
+  } else {
+    next()
+  }
+})
+
+app.post('/people/:person/addPhoto', function (req, res, next) {
+  var person = req.params.person.toLowerCase()
+  if (peopleData[person]) {
+    console.log("  - req.body:", req.body)
+    if (req.body && req.body.url && req.body.caption) {
+      peopleData[person].photos.push({
+        caption: req.body.caption,
+        url: req.body.url
+      })
+      console.log("  - peopleData[" + person + "]:", peopleData[person])
+      fs.writeFile(
+        "./peopleData.json",
+        JSON.stringify(peopleData, null, 2),
+        function (err) {
+          if (!err) {
+            res.status(200).send()
+          } else {
+            res.status(500).send("Error: error saving photo card")
+          }
+        }
+      )
+    } else {
+      res.status(400).send("Error: request body needs a 'url' and 'caption'")
+    }
   } else {
     next()
   }
